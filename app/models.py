b26facdadb9f11939b390ab88ec26391a0c2344d
Your Name Nobody <you@example.com>
2016-10-03 13:49:09 -0400

    use ModelAdmin

---
+++ app/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+class Speaker(models.Model):
+    name = models.CharField(max_length=40)
+    title = models.CharField(max_length=50)
+    bio = models.TextField(max_length=1000)
+    twitter = models.CharField(max_length=16, blank=True)
+    facebook = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Track(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.title
+
+SESSION_STATUSES = (
+    ('a', 'Approved'),
+    ('s', 'Submitted'),
+    ('r', 'Rejected'),
+)
+class Session(models.Model):
+    title = models.CharField(max_length=50)
+    abstract = models.TextField(max_length=2000)
+    track = models.ForeignKey(Track)
+    speaker = models.ForeignKey(Speaker)
+    status = models.CharField(max_length=1, choices=SESSION_STATUSES, default='s')
+
+    def __str__(self):
+        return self.title
